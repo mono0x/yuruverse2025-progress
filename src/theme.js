c74c162ab6be19bfbb24ba58a169396c9e9ba676
mono <mono@mono0x.net>
2020-07-06 02:06:22 +0900

    Reduce horizontal padding of tables

--- src/theme.js
+++ src/theme.js
@@ -17,6 +17,16 @@ const theme = createMuiTheme({
       default: "#fff",
     },
   },
+  overrides: {
+    MuiTableCell: {
+      root: {
+        padding: "16px 8px 16px 8px",
+      },
+      sizeSmall: {
+        padding: "6px 12px 6px 8px",
+      },
+    },
+  },
 })
 
 export default theme
