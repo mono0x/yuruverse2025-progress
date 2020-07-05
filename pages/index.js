a3e833201e139cae3a410d5622d7786def566ff8
mono <mono@mono0x.net>
2020-07-06 00:39:27 +0900

    Add table and pagination

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,19 @@
-import { MenuItem, Select } from "@material-ui/core"
+import {
+  AppBar,
+  Container,
+  FormHelperText,
+  MenuItem,
+  Select,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableFooter,
+  TableHead,
+  TablePagination,
+  TableRow,
+  Typography,
+} from "@material-ui/core"
 import fs from "fs"
 import palette from "google-palette"
 import path from "path"
@@ -10,29 +25,52 @@ export default function IndexPage(props) {
 
   const [kind, setKind] = useState("LOCAL")
 
+  const [rowsPerPage, setRowsPerPage] = useState(10)
+  const [page, setPage] = useState(0)
+
   const filtered = useMemo(() => {
     const filtered = items.filter(item => item.character.kind == kind)
     filtered.sort(
       (a, b) =>
-        b.records[b.records.length - 1].point -
-        a.records[a.records.length - 1].point
+        a.records[a.records.length - 1].rank -
+        b.records[b.records.length - 1].rank
     )
-    return filtered.slice(0, 10)
+    return filtered
   }, [items, kind])
+
+  const paginated = useMemo(() => {
+    return filtered.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+  }, [filtered, page, rowsPerPage])
+
+  const oneRankHigher = useMemo(() => {
+    if (page === 0) {
+      return [
+        null,
+        ...filtered.slice(
+          page * rowsPerPage,
+          page * rowsPerPage + rowsPerPage - 1
+        ),
+      ]
+    }
+    return filtered.slice(
+      page * rowsPerPage - 1,
+      page * rowsPerPage + rowsPerPage - 1
+    )
+  }, [filtered, page, rowsPerPage])
 
   const dates = useMemo(() => {
     const dates = new Set()
-    filtered
+    paginated
       .flatMap(item => item.records.map(record => record.date))
       .forEach(date => dates.add(date))
     return Array.from(dates)
-  }, [filtered])
+  }, [paginated])
 
   const data = useMemo(() => {
-    const colors = palette("mpn65", filtered.length).map(hex => `#${hex}`)
+    const colors = palette("mpn65", paginated.length).map(hex => `#${hex}`)
     return {
       labels: dates,
-      datasets: filtered.map((item, i) => ({
+      datasets: paginated.map((item, i) => ({
         label: item.character.name,
         borderColor: colors[i],
         fill: false,
@@ -54,16 +92,80 @@ export default function IndexPage(props) {
         },
       },
     }
-  }, [filtered, dates])
+  }, [paginated, dates])
 
   return (
-    <div>
-      <Select value={kind} onChange={e => setKind(e.target.value)}>
+    <Container disableGutters={true} maxWidth={false}>
+      <AppBar position="static">
+        <Typography variant="h6">YuruGP 2020 Progress</Typography>
+      </AppBar>
+      <Select
+        value={kind}
+        onChange={e => {
+          setKind(e.target.value)
+          setPage(0)
+        }}
+      >
         <MenuItem value="LOCAL">ご当地</MenuItem>
         <MenuItem value="COMPANY">企業・その他</MenuItem>
       </Select>
+      <FormHelperText>Filter</FormHelperText>
       <Line data={data} />
-    </div>
+      <TableContainer>
+        <Table size="small">
+          <TableHead>
+            <TableRow>
+              <TableCell align="right">Rank</TableCell>
+              <TableCell>Name</TableCell>
+              <TableCell align="right">Total Points</TableCell>
+              <TableCell align="right">+ Points</TableCell>
+              <TableCell align="right">Behind</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {paginated.map((item, i) => (
+              <TableRow key={item.character.id}>
+                <TableCell align="right">
+                  {item.records[item.records.length - 1].rank}
+                </TableCell>
+                <TableCell>{item.character.name}</TableCell>
+                <TableCell align="right">
+                  {item.records[item.records.length - 1].point}
+                </TableCell>
+                <TableCell align="right">
+                  {item.records.length >= 2
+                    ? item.records[item.records.length - 1].point -
+                      item.records[item.records.length - 2].point
+                    : "-"}
+                </TableCell>
+                <TableCell align="right">
+                  {oneRankHigher[i] !== null
+                    ? oneRankHigher[i].records[
+                        oneRankHigher[i].records.length - 1
+                      ].point - item.records[item.records.length - 1].point
+                    : "-"}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+          <TableFooter>
+            <TableRow>
+              <TablePagination
+                count={items.length}
+                rowsPerPageOptions={[10, 25, 50]}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                onChangeRowsPerPage={e => {
+                  setRowsPerPage(e.target.value)
+                  setPage(0)
+                }}
+                onChangePage={(_, newPage) => setPage(newPage)}
+              />
+            </TableRow>
+          </TableFooter>
+        </Table>
+      </TableContainer>
+    </Container>
   )
 }
 
