a69d1d82cc73f2c42b78312e3f076d33d4c84e05
mono <mono@mono0x.net>
2020-07-06 12:30:13 +0900

    Add Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,25 @@
 import CssBaseline from "@material-ui/core/CssBaseline"
 import { ThemeProvider } from "@material-ui/core/styles"
 import Head from "next/head"
+import Router from "next/router"
 import React from "react"
 
 import theme from "../src/theme"
 
+import * as gtag from "../src/gtag"
+
 export default function MyApp(props) {
   const { Component, pageProps } = props
+
+  React.useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    Router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      Router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [])
 
   React.useEffect(() => {
     // Remove the server-side injected CSS.
--- pages/_document.js
+++ pages/_document.js
@@ -2,6 +2,7 @@ import { ServerStyleSheets } from "@material-ui/core/styles"
 import Document, { Head, Html, Main, NextScript } from "next/document"
 import React from "react"
 
+import { GA_TRACKING_ID } from '../src/gtag'
 import theme from "../src/theme"
 
 export default class MyDocument extends Document {
@@ -14,6 +15,23 @@ export default class MyDocument extends Document {
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          />
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
           />
         </Head>
         <body>
