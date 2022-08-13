acdedb95db8dff90e44632020c6bfc2264972574
mono <mono@mono0x.net>
2022-08-14 02:28:42 +0900

    Introduce Cloudflare Web Analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -33,6 +33,13 @@ export default class MyDocument extends Document {
           `,
             }}
           />
+          {/* Cloudflare Web Analytics */}
+          <script
+            defer
+            src="https://static.cloudflareinsights.com/beacon.min.js"
+            data-cf-beacon='{"token": "b38b408d1b114b8a9c3c1ad3fccf5791"}'
+          />
+          {/* End Cloudflare Web Analytics */}
         </Head>
         <body>
           <Main />
