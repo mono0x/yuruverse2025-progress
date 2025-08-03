f38ad6c912314ca293407baee616020b78ed4a8e
mono <mono@mono0x.net>
2025-08-04 00:38:59 +0900

    Simplify the mui theme

--- src/theme.ts
+++ src/theme.ts
@@ -1,4 +1,3 @@
-import { red } from "@mui/material/colors"
 import { createTheme } from "@mui/material/styles"
 import { Roboto } from "next/font/google"
 
@@ -11,39 +10,6 @@ const roboto = Roboto({
 const theme = createTheme({
   typography: {
     fontFamily: roboto.style.fontFamily,
-  },
-  palette: {
-    primary: {
-      main: "#556cd6",
-    },
-    secondary: {
-      main: "#19857b",
-    },
-    error: {
-      main: red.A400,
-    },
-    background: {
-      default: "#fff",
-    },
-  },
-  components: {
-    MuiTableCell: {
-      styleOverrides: {
-        root: {
-          padding: "16px 4px 16px 4px",
-        },
-        sizeSmall: {
-          padding: "6px 6px 6px 4px",
-        },
-      },
-    },
-    MuiTypography: {
-      styleOverrides: {
-        h1: {
-          fontSize: 36,
-        },
-      },
-    },
   },
 })
 
